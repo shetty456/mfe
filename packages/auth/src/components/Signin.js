7469452cfb172d42fba1d12366ea846a272bf46b
Sunil Hanamshetty <64653515+shetty456@users.noreply.github.com>
2024-01-10 18:40:19 +0530

    add dashboard app

--- packages/auth/src/components/Signin.js
+++ packages/auth/src/components/Signin.js
@@ -17,7 +17,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" to="/">
-        Your Website
+        Shetty's App
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
